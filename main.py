03348d0d7e275a955e270f8de6c301c3c34c17c9
wahyubajax <yuyuu358@gmail.com>
2025-03-24 07:10:32 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyrogram import Client,  filters
+from pyrogram import Client
+import loging
+import os
+
+
+app = Client(
+  name"debug",
+  api_id=API_HASH,
+  api_hash=API)_HASH,
+  bot_token=BOT_TOKEN,
+)
