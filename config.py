b70f47ae06e2749db474dde2d7c86ba81cd550cc
wahyubajax <yuyuu358@gmail.com>
2025-03-24 07:52:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
 
+API_ID = int(os.getenv("API_ID", "21916107"))
+API_HASH = os.getenv("API_HASH", "772494d5fd3dd2ef55e02dc965ecacea")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7625679291:AAHXMBzrCmKqqyjW2Vi_ODkua_PDpYcSleo")
+OWNER_ID = int(os.getenv("OWNER_ID", "5654247585"))
