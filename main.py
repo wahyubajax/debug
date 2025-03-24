d58c508951dd35cf2fd58bdc0457ef8918f4e356
wahyubajax <yuyuu358@gmail.com>
2025-03-24 08:11:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ from pyrogram import Client
 import aiorun
 import os
 
-class App(Client):
-  def __init__():
-     super().__init__(
-       name="debug",
-       api_id=API_ID,
-       api_hash=API_HASH,
-       bot_token=BOT_TOKEN,
-       plugins=dict(root="debug.plugins"),
-       worker='50',
+app - Client(
+    name="debug",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="debug.plugins"),
+    worker='50',
 )
 
 async def start_client():
-    await super().start()
+    await app.start()
     me = await self.get.me()
     self.username = "@" + me.username
     print(f"{me.fist_name} start {me.username}")
@@ -23,12 +21,12 @@ async def start_client():
 
 
 async def stop_client():
-    await super().stop()
+    await app.stop()
     print("bot stop bye")
 
 
 if __name__ == "__main__":
-   aiorun.run(start_client(),
-   shutdown_callback=stop_client())
+   aiorun.run(start_client(), 
+   shutdown_callback=stop_client()0
 
     
