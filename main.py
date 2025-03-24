5d5bb5d75911c93faa0cc1bd1df7e862ea323075
wahyubajax <yuyuu358@gmail.com>
2025-03-24 08:01:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class App(Client):
        api_id=API_ID,
        api_hash=API_HASH,
        bot_token=BOT_TOKEN,
-       plugins=dict(root="debug.plugins")
+       plugins=dict(root="debug.plugins"),
        worker='50',
 )
 
