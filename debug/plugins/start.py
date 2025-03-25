a1943f92dc2a5a88a0d30e95518a773afd011337
wahyubajax <yuyuu358@gmail.com>
2025-03-26 03:17:41 +0800

    Update start.py

--- debug/plugins/start.py
+++ debug/plugins/start.py
@@ -2,6 +2,6 @@ from config import *
 from pyrogram import Client, filters
 from main import app
 
-@app.on_message(filters.command("start") & filter.private)
+@app.on_message(filters.command("start") & filters.private)
 async def start_cmd(bot, message):
   print("hi bang")
