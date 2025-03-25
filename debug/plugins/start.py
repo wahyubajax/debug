993028b41249d47d6d3d4aff7f934242adeffe36
wahyubajax <yuyuu358@gmail.com>
2025-03-26 03:21:15 +0800

    Delete debug/plugins/start.py

--- debug/plugins/start.py
+++
@@ -1,7 +0,0 @@
-from config import * 
-from pyrogram import Client, filters
-from pyrogram import Client
-
-@Client.on_message(filters.command("start") & filters.private)
-async def start_cmd(bot, message):
-  print("hi bang")
