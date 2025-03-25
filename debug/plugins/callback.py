d96c0854ac59f8f90e0bfe80305baf042ebac04f
wahyubajax <yuyuu358@gmail.com>
2025-03-25 17:17:37 +0800

    Update callback.py

--- debug/plugins/callback.py
+++ debug/plugins/callback.py
@@ -1,1 +1,8 @@
+from config import *
+from pyrogram import Client, filters 
+from main import app
+import os
 
+@app.on_callback_quaery()
+async def callback_quaery(bot, message):
+  await message.delete()
