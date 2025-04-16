af964c4b411ad24e0ebf60c09260ffdbf6180d7c
wahyubajax <yuyuu358@gmail.com>
2025-04-16 18:04:21 +0800

    Update inline.py

--- debug/plugins/inline.py
+++ debug/plugins/inline.py
@@ -2,3 +2,9 @@ from pyrogram import Client, filters
 
 @client.on_message(filters.command("start"))
 async start_cmd((client, message):
+welcome_teks= """"
+
+perintah 
+/addubot
+
+ await reply_to(welcome_teks)
